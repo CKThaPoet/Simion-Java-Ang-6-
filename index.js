3dbdfe6ed801df2902f415b9cee4679d5360f227
Michael Carson <michael.carson80@yahoo.com>
2021-09-15 12:22:10 -0400

    progress so far

---
+++ index.js
@@ -0,0 +1,29 @@
+const topLeft = document.querySelector('.top-left-panel');
+const topRight = document.querySelector('.top-right-panel');
+const bottomLeft = document.querySelector('.bottom-left-panel');
+const bottomRight = document.querySelector('.bottom-right-panel');
+
+
+const sequences = [topLeft, topRight, bottomLeft, bottomRight];
+
+const flash = (panel) => {
+    return new Promise((resolve, reject) => {
+        panel.className += ' active';
+        setTimeout(() => {
+            panel.className = panel.className.replace(
+                ' active', 
+                ''
+            );
+            resolve();
+        }, 1000)
+    });
+};
+
+const main = async () => {
+    for(const panel of sequence){
+        await flash(panel);
+    }
+   
+};
+
+main();
